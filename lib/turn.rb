0d97491707af72304629e21c219ce2b194dde227
abstract-animation-9914 <vm930@hotmail.com>
2019-02-08 03:21:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,20 @@
-def board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(input)
+   input.to_i - 1
 end 
+
+def move(board, board_index, character = "X")
+  board[board_index] = character
+end 
+
 
 
 
