9c0e15b48a1d75ded43892a7c3fb2281bd92f774
abstract-animation-9914 <vm930@hotmail.com>
2019-02-08 04:03:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.chomp
-  index = input.to_i - 1
+  index = input_to_index(input)
     if valid_move?(board, index)
       move(board, index)
       display_board(board)
