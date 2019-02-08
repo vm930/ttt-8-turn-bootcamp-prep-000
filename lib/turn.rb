e48917f77bdcc1225a674594a97bc139341223c8
abstract-animation-9914 <vm930@hotmail.com>
2019-02-08 03:15:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,6 +11,6 @@ if move_is_valid?
   return board
 else 
   puts "Please provide a position that is valid"# If the move is invalid, ask for a new move until a valid move is received.
-
+end
 end 
 
