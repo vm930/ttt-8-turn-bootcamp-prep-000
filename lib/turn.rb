f1bdd00ddcd9f3dd5364ac26122b516d8801c00b
abstract-animation-9914 <vm930@hotmail.com>
2019-02-08 03:11:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,8 @@
-def turn
-  
-end 
-
-# Asking the user for their move by position 1-9.
+def turn()
+  # Asking the user for their move by position 1-9.
 # Receiving the user input.
 # Convert position to an index.
 # If the move is valid, make the move and display the board to the user.
 # If the move is invalid, ask for a new move until a valid move is received.
+end 
+
